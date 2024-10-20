93205585cad083079f267f2b54044a375ed44faf
Harshit sharma <programming966051@gmail.com>
2024-10-20 15:13:14 +0530

    add more type of data

--- src/store/image.js
+++ src/store/image.js
@@ -10,7 +10,12 @@ export const useProductsStore = defineStore('products', {
         formats: '.obj .png .jpg',
         image: 'https://images.pexels.com/photos/1350789/pexels-photo-1350789.jpeg?auto=compress&cs=tinysrgb&w=600',
         off: '30% off',
-        rate: 'Free'
+        rate: 'Free',
+        color: 'Brown',
+        material: 'Bamboo',
+        fileType: '.obj',
+        texture: 'Wood',
+        type: 'Classic'
       },
       {
         id: 2,
@@ -18,8 +23,12 @@ export const useProductsStore = defineStore('products', {
         formats: '.obj .png .jpg',
         image: 'https://images.pexels.com/photos/276583/pexels-photo-276583.jpeg?auto=compress&cs=tinysrgb&w=600',
         off: '20% off',
-        material:"wood",
-        color:"gary"
+        rate: '$150.00',
+        color: 'Gray',
+        material: 'Wood',
+        fileType: '.obj',
+        texture: 'Fabric',
+        type: 'Modern'
       },
       {
         id: 3,
@@ -27,90 +36,125 @@ export const useProductsStore = defineStore('products', {
         formats: '.obj .png .jpg',
         image: 'https://images.pexels.com/photos/2995012/pexels-photo-2995012.jpeg?auto=compress&cs=tinysrgb&w=600',
         off: '15% off',
-        rate: '$50.00'
+        rate: '$50.00',
+        color: 'Brown',
+        material: 'Wood',
+        fileType: '.obj',
+        texture: 'Wood',
+        type: 'Classic'
       },
-      
       {
         id: 4,
-        title: 'Caopm',
+        title: 'Caopm Chair',
         formats: '.obj .png .jpg',
         image: 'https://images.pexels.com/photos/945688/pexels-photo-945688.jpeg?auto=compress&cs=tinysrgb&w=600',
         off: '15% off',
-        rate: '$50.00'
+        rate: '$50.00',
+        color: 'Black',
+        material: 'Leather',
+        fileType: '.obj',
+        texture: 'Leather',
+        type: 'Modern'
       },
-      
       {
         id: 5,
-        title: 'Wooden Table',
+        title: 'Wooden Dining Table',
         formats: '.obj .png .jpg',
         image: 'https://images.pexels.com/photos/356048/pexels-photo-356048.jpeg?auto=compress&cs=tinysrgb&w=600',
         off: '15% off',
-        rate: '$50.00'
+        rate: '$120.00',
+        color: 'Brown',
+        material: 'Wood',
+        fileType: '.obj',
+        texture: 'Wood',
+        type: 'Classic'
       },
-      
       {
         id: 6,
-        title: 'Wooden Table',
+        title: 'Coffee Table',
         formats: '.obj .png .jpg',
         image: 'https://images.pexels.com/photos/534151/pexels-photo-534151.jpeg?auto=compress&cs=tinysrgb&w=600',
-        rate: "Free"
+        rate: 'Free',
+        color: 'White',
+        material: 'Plastic',
+        fileType: '.obj',
+        texture: 'Plastic',
+        type: 'Modern'
       },
-      
       {
         id: 7,
         title: 'Table Lighting',
         formats: '.obj .png .jpg',
         image: 'https://images.pexels.com/photos/1668860/pexels-photo-1668860.jpeg?auto=compress&cs=tinysrgb&w=600',
         off: '15% off',
-        rate: '$22.00'
+        rate: '$22.00',
+        color: 'Yellow',
+        material: 'Metal',
+        fileType: '.obj',
+        texture: 'Metal',
+        type: 'Modern'
       },
-      
       {
         id: 8,
-        title: 'Kitchen carft',
+        title: 'Kitchen Craft',
         formats: '.obj .png .jpg',
         image: 'https://images.pexels.com/photos/2724749/pexels-photo-2724749.jpeg?auto=compress&cs=tinysrgb&w=600',
-       
-        rate: '$28.00'
+        rate: '$28.00',
+        color: 'Silver',
+        material: 'Stainless Steel',
+        fileType: '.obj',
+        texture: 'Metal',
+        type: 'Modern'
       },
-      
       {
         id: 9,
-        title: 'markof',
+        title: 'Markof Chair',
         formats: '.obj .png .jpg',
         image: 'https://images.pexels.com/photos/1556704/pexels-photo-1556704.jpeg?auto=compress&cs=tinysrgb&w=600',
-     
-        rate: '$50.00'
+        rate: '$50.00',
+        color: 'Gray',
+        material: 'Fabric',
+        fileType: '.obj',
+        texture: 'Fabric',
+        type: 'Modern'
       },
-      
       {
         id: 10,
-        title: 'lamp',
+        title: 'Classic Lamp',
         formats: '.obj .png .jpg',
         image: 'https://images.pexels.com/photos/2233416/pexels-photo-2233416.jpeg?auto=compress&cs=tinysrgb&w=600',
         off: '15% off',
-        rate: '$50.00'
+        rate: '$50.00',
+        color: 'Black',
+        material: 'Metal',
+        fileType: '.obj',
+        texture: 'Metal',
+        type: 'Classic'
       },
-
       {
         id: 11,
-        title: 'lamp',
+        title: 'Modern Lamp',
         formats: '.obj .png .jpg',
         image: 'https://images.pexels.com/photos/813691/pexels-photo-813691.jpeg?auto=compress&cs=tinysrgb&w=600',
-       
-        rate: '$50.00'
+        rate: '$50.00',
+        color: 'White',
+        material: 'Plastic',
+        fileType: '.obj',
+        texture: 'Plastic',
+        type: 'Modern'
       },
-
       {
         id: 12,
-        title: 'lamp',
+        title: 'Stylish Lamp',
         formats: '.obj .png .jpg',
         image: 'https://images.pexels.com/photos/1629998/pexels-photo-1629998.jpeg?auto=compress&cs=tinysrgb&w=600',
-        
-        rate: 'Free'
+        rate: 'Free',
+        color: 'Silver',
+        material: 'Metal',
+        fileType: '.obj',
+        texture: 'Metal',
+        type: 'Modern'
       },
-
-
     ],
   }),
   getters: {
