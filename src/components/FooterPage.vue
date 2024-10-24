d6c0d412a46d5ec502ee9eb3e8e078b81903ec6b
Harshit sharma <programming966051@gmail.com>
2024-10-24 15:08:07 +0530

    working on footer

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -10,6 +10,24 @@
         Foyr is an online interior design software that allows users to create
         3D floor plans and interior designs for homes and offices.
       </div>
+      <div class="icons">
+        <img src="../assets/fi_facebook.png" alt="" />
+        <img src="../assets/fi_instagram.png" alt="" />
+        <img src="../assets/fi_twitter.png" alt="" />
+        <img src="../assets/fi_linkedin.png" alt="" />
+        <img src="../assets/fi_youtube.png" alt="" />
+      </div>
+    </div>
+    <div class="second-div">
+      <div class="legal-link">Legal Links</div>
+      <div class="links">
+        <ul>
+          <li>Delivery Policy</li>
+          <li>privacy Policy</li>
+          <li>About</li>
+          <li>Term of services</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -27,12 +45,14 @@ export default {
   opacity: 0px;
   background: #f4f0ef;
   display: flex;
+  justify-content: space-evenly;
+
   .first-div {
     display: flex;
     flex-direction: column;
     background-color: #ffffff;
     width: 17rem;
-    height: 14.375rem;
+    height: 13.375rem;
     gap: 24px;
     opacity: 0px;
     .img-section {
@@ -72,7 +92,52 @@ export default {
       line-height: 18.2px;
       text-align: left;
       color: #939090;
+    }
+    .icons {
+      width: 4rem;
+      height: 1.5rem;
+      gap: 11px;
+      display: flex;
+    }
+  }
+ 
+  .second-div {
+    font-family: Inter;
+    background-color: #ffffff;
+    padding: 1rem;
+    width: 17rem;
+    height: 13.375rem;
+    .legal-link {
+      //styleName: Body Big/M;
+      font-family: Inter;
+      font-size: 18px;
+      font-weight: 500;
+      line-height: 23.4px;
+      text-align: left;
+      color: #484646;
 
+    }
+
+    .links {
+      margin-top: 2.5rem;
+      ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+
+        li {
+          font-size: 14px;
+          font-weight: 400;
+          line-height: 18.2px;
+          color: #939090;
+          margin: 0.5rem 0;
+
+          &:hover {
+            color: #7343ea;
+            text-decoration: underline;
+          }
+        }
+      }
     }
   }
 }
