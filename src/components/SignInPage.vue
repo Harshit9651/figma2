b6ec9567ce66062af21497b7677be4134a8676c1
Harshit sharma <programming966051@gmail.com>
2024-11-15 19:17:59 +0530

    add homare

--- src/components/SignInPage.vue
+++ src/components/SignInPage.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="main-wrapper">
     <div class="neo-green"></div>
+    <div class="logo">
+      <img src="../assets/Frame 22.png" alt="" />
+    </div>
   </div>
 </template>
 <script setup></script>
@@ -11,8 +14,19 @@
   .neo-green {
     width: 100%;
     height: 1rem;
-    padding: 12px 10px 12px 10px;
-    gap: 10px;
+    background: #dcf763;
+  }
+  .logo {
+    width: 12rem;
+    height: 3.5rem;
+    top: 51px;
+    left: 63px;
+    gap: 12px;
+    background-color: aquamarine;
+    img{
+      height: 100%;
+      width: 100%;
+    }
   }
 }
 </style>
