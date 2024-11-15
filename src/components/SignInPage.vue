6fabafd3a8da7cf13ed7fbaa58788667a764cbc4
Harshit sharma <programming966051@gmail.com>
2024-11-15 19:59:12 +0530

    making data sets

--- src/components/SignInPage.vue
+++ src/components/SignInPage.vue
@@ -1,32 +1,48 @@
 <template>
   <div class="main-wrapper">
     <div class="neo-green"></div>
-    <div class="logo">
+  
+    <div class="content-wrapper">
+      <div class="logo">
       <img src="../assets/Frame 22.png" alt="" />
+    </div>
     </div>
   </div>
 </template>
 <script setup></script>
-<style lang="scss" scoped>
+ <style lang="scss" scoped>
 .main-wrapper {
   height: 100vh;
   width: 100vw;
+  display: flex;
+  flex-direction: column;
+ align-items: center;
+
+
+
   .neo-green {
     width: 100%;
     height: 1rem;
     background: #dcf763;
+    
   }
-  .logo {
+ 
+  .content-wrapper{
+    margin-top: 2rem;
+    width: 80%;
+    border: 2px solid black;
+    height:95%;
+    .logo {
     width: 12rem;
     height: 3.5rem;
-    top: 51px;
-    left: 63px;
-    gap: 12px;
-    background-color: aquamarine;
+    margin: 2rem;
+    
     img{
       height: 100%;
       width: 100%;
     }
   }
-}
+  }
+} 
+
 </style>
