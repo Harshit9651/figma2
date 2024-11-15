7e3e07337bec7eaa882197039d06ee910eba5d0e
Harshit sharma <programming966051@gmail.com>
2024-11-15 19:06:53 +0530

    making neo-green

--- src/components/SignInPage.vue
+++ src/components/SignInPage.vue
@@ -1,11 +1,18 @@
 <template>
-    <div class="main-wrapper">
-        
-    </div>
+  <div class="main-wrapper">
+    <div class="neo-green"></div>
+  </div>
 </template>
-<script setup>
-
-</script>
+<script setup></script>
 <style lang="scss" scoped>
-
+.main-wrapper {
+  height: 100vh;
+  width: 100vw;
+  .neo-green {
+    width: 100%;
+    height: 1rem;
+    padding: 12px 10px 12px 10px;
+    gap: 10px;
+  }
+}
 </style>
